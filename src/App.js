20f56dd488e8b427a99037a8d9261e3f9ab65527
xu1jia2qi3 <jiaqy@live.com>
2019-10-10 12:32:05 -0400

    /

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
                 <Link to ="/">Home</Link>
                 <Link to ="/resume">Resume</Link>
                 <Link to ="/aboutme">About Me</Link>
-                <Link to ="projects">Projects</Link>
-                <Link to ="contact">Contact</Link>
+                <Link to ="/projects">Projects</Link>
+                <Link to ="/contact">Contact</Link>
             </Navigation>
         </Header>
         <Drawer title="Jiaqi">
@@ -22,8 +22,8 @@ function App() {
                  <Link to ="/">Home</Link>
                 <Link to ="/resume">Resume</Link>
                 <Link to ="/aboutme">About Me</Link>
-                <Link to ="projects">Projects</Link>
-                <Link to ="contact">Contact</Link>
+                <Link to ="/projects">Projects</Link>
+                <Link to ="/contact">Contact</Link>
             </Navigation>
         </Drawer>
     
