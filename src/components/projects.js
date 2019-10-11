f0efcad778a73aea423785c8a89583e2434ade02
xu1jia2qi3 <jiaqy@live.com>
2019-10-10 21:03:50 -0400

    add project components

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,97 @@
 import React, {Component} from 'react';
-
+import {Tabs, Tab, Grid,Cell,Card,CardTitle,CardActions,Button,CardMenu,CardText,IconButton} from 'react-mdl';
 
 class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {activeTab : 0};
+    }
+
+    toggleCategories(){
+        if(this.state.activeTab === 0){
+            return(
+                <div className="projects-grid">
+                    <Card shadow={5} style={{width: '450px', margin: 'auto',borderRadius: '10px'}}>
+                        <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Welcome1</CardTitle>
+                        <CardText>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>Github</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                    <Card shadow={5} style={{width: '450px', margin: 'auto',borderRadius: '10px'}}>
+                        <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Welcome2</CardTitle>
+                        <CardText>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>Github</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                    <Card shadow={5} style={{width: '450px', margin: 'auto',borderRadius: '10px'}}>
+                        <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Welcome3</CardTitle>
+                        <CardText>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                        </CardText>
+                        <CardActions border>
+                            <Button colored>Github</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                </div>
+            )
+        } else if (this.state.activeTab === 1){
+            return(
+                <div>this is angular</div>
+            )
+        } else if (this.state.activeTab === 2){
+            return(
+                <div>this is vue</div>
+            )
+        } else if (this.state.activeTab === 3){
+            return(
+                <div>this is mysql</div>
+            )
+        } 
+    }
+    
+
     render() {
         return(
-        <div><h1>Projects Page</h1></div>)
+        <div className = "category-tabs">
+            <Tabs activeTab = {this.state.activeTab} onChange = {(tabId) => this.setState({activeTab:tabId})} ripple>
+                <Tab>React</Tab>
+                <Tab>angular</Tab>
+                <Tab>vue</Tab>
+                <Tab>mysql</Tab>
+            </Tabs>
+            <section>
+                <Grid>
+                    <Cell col={12}>
+                        <div className = "content">
+                          {this.toggleCategories()}
+                        </div>
+                    </Cell>
+                </Grid>
+            </section>
+
+        </div>
+        )
     }
 }
 
